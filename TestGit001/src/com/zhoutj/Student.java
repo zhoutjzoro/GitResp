e5170bae21ef2249ca862607905fff54aef86f66
zhoutj <827255828@qq.com>
2023-11-12 08:43:56 +0800

    添加Student.java文件

---
+++ TestGit001/src/com/zhoutj/Student.java
@@ -0,0 +1,22 @@
+package com.zhoutj;
+
+public class Student extends Person {
+    private String address;
+    private  String telPhone;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getTelPhone() {
+        return telPhone;
+    }
+
+    public void setTelPhone(String telPhone) {
+        this.telPhone = telPhone;
+    }
+}
